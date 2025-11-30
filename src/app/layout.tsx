c0e77360db34f5235f6e40a07000167e491c355e
dangtuandat123 <datxtofficial@gmail.com>
2025-11-30 21:26:56 +0700

    chore(init): scaffold nextjs project with prisma and tailwind

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Happy Land Real Estate - Bất Động Sản TP. Thủ Đức",
+    description: "Hệ thống phân phối và giao dịch bất động sản uy tín hàng đầu tại TP. Thủ Đức",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="vi">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
